19e91c6f10c1b31a1e512882bd3bd1c97415b95f
yogaardiansyah <yogaardiansyah@protonmail.com>
2024-06-01 21:59:46 +0700

    Updates

---
+++ precache-manifest.f0ab78f0fc714249c24e515cabfc151b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94fcb8dff0f2f59815bff1eeeffdea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7d6b9f8978281162c50d",
+    "url": "/static/js/2.fd5371a8.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
